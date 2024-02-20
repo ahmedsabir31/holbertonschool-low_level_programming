a76a4d2c2ff886242e05d155df0184c2780c03cd
ahmedsabir31 <ahmed.sbr31@gmail.com>
2024-02-20 01:56:14 -0800

    positibenegative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - main block
- * This program will assign a random number to the variable n
- * betty style doc for function main goes there
- * Return: 0 */
+ *  * main - Entry point
+ *   * Description: This program will assign a random number to the variable n
+ *    * according to Betty coding style and documentation.
+ *     * Return: 0 (Success)
+ *      */
 int main(void)
 {
-	int n;
+	    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+	        srand(time(0));
+		    n = rand() - RAND_MAX / 2;
+
+		        if (n > 0)
+				    {
+					            printf("%d is positive\n", n);
+						        }
+			    else if (n == 0)
+				        {
+						        printf("%d is zero\n", n);
+							    }
+			        else
+					    {
+						            printf("%d is negative\n", n);
+							        }
+
+				    return (0);
 }
