2244b6543ecec20c8dbda6cf1eb93c9a2821d935
ahmedsabir31 <ahmed.sbr31@gmail.com>
2024-02-21 01:57:04 -0800

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * grints all simple digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 0; num++)
+{
+putchar ("%d", num);
+}
+putchar("\n");
+return 0;
+}
