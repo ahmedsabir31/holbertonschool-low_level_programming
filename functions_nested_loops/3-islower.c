94c957a5281eb72ae1ae6cacfbca9b9affe0b2cd
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-22 19:01:05 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * check lowercase characters
+ * Write a function that checks for lowercase character.
+ * Return: void
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
