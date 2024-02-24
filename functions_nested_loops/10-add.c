801f22a3997f4b19a1705018d93e772e14e7d4f2
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-24 08:43:07 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add two integers
+ * @o: int first
+ * @g: int second
+ * Return: void
+ */
+int add(int o, int g)
+{
+int o;
+int g;
+int result;
+result = x + y;
+{
+_putchar(result + '0');
+}
+return (result);
+}
