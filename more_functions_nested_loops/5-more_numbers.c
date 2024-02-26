26bb303ae60647cc7c2348c59c0d1bd7d2ac0d1b
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-26 16:30:55 +0000

    5more

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,16 +3,16 @@
  *print_numbers - A function that prints the numbers, from 0 to 9
  * Return: void
  */
-void more_numbers(void);
+void more_numbers(void)
 int a, b;
-	for (a = 0; a < 10; a++)
-	{
-		for (b = 0; b < 15; b++)
-		{
-			if (b > 9)
-			_putchar(b / 10 + '0');
-			_putchar(b % 10 + '0');
-		}
-		_putchar('\n');
-	}
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 15; b++)
+{
+if (b > 9)
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+}
+_putchar('\n');
+}
 }
