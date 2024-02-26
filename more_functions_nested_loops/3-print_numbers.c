4df2083dc1ebfdc0590febba2763df7b0a53ce6f
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-26 10:52:55 +0000

    3print

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - A function that prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num;
+for (num = 0 ; num <= 9 ; i++)
+_putchar (i)
+}
+_putchar ('\n');
