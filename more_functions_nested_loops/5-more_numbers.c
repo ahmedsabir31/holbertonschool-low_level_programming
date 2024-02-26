12797c36c90730e3b687326fdbb6ec04a8c42d68
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-26 16:21:18 +0000

    5more

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,14 +4,15 @@
  * Return: void
  */
 void more_numbers(void);
-int a , int b;
-for (a = 0 ; a < 10; a++);
+int a , b;
+for (a = 0; a <= 9; a++)
 {
-for (b = 0 ; b < 15; b++);
+for (b = 0; b <= 14; b++)
 {
 if (b > 9)
-_putchar(b / 10 + '0');
-_putchar(b % 10 + '0');
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
 }
 _putchar('\n');
+}
 }
