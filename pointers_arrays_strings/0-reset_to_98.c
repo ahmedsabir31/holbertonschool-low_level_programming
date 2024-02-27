ae765957af6810c36ac1703abd08273e6f08f0d7
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-27 12:20:18 +0000

    098

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *reset_to_98 - A function that takes a pointer to an int 
+ *reset_to_98 - A function that takes a pointer to an int
  *
  *@n: the pointer to an int
  *Return: void
