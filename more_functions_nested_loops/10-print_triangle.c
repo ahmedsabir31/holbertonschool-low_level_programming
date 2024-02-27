129c8bb4a2e43a3b612392e5ab1b010081832a46
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-27 10:24:59 +0000

    triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,22 +2,23 @@
 /**
  * print_triangle - Function to print a triangle of '#' characters
  * @size: Size of the triangle
+ * Return: void
  */
 void print_triangle(int size)
 {
-int i, j;
+	int i;
+	int j;
 
-if (size <= 0)
-{
-_putchar('\n');
-}
+	if (size <= 0)
+		_putchar('\n');
 
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+			_putchar(' ');
+		for (j = 1; j <= i; j++)
+			_putchar('#');
+			
+	_putchar('\n');
+	}
 }
