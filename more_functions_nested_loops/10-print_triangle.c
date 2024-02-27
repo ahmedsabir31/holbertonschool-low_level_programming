1fe266b468ebb1676695c7ceb8aae9e30b29fdb4
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-27 10:27:38 +0000

    triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,19 @@
  */
 void print_triangle(int size)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	if (size <= 0)
-		_putchar('\n');
+if (size <= 0)
+_putchar('\n');
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size - i; j++)
-			_putchar(' ');
-		for (j = 1; j <= i; j++)
-			_putchar('#');
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size - i; j++)
+_putchar(' ');
+for (j = 1; j <= i; j++)
+_putchar('#');
 			
-	_putchar('\n');
-	}
+_putchar('\n');
+}
 }
