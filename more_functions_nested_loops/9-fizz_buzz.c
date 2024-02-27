75504347e560b6645edc868ecb0dcb31f1c6c9b4
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-27 09:12:54 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
 /**
- * fizz_buzz - Program that prints the numbers from 1 to 100
+ * main - main block
  */
-void fizz_buzz()
+int main(void)
 {
-int a;
-for (a = 1; a <= 100; a++)
-{
-if (a % 3 == 0 && a % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (a % 3 == 0)
-{
-printf("Fizz");
-}
-else if (a % 5 == 0)
-{
-printf("Buzz");
-}
-else
-{
-printf("%d", a);
-}
-}
-printf("\n");
-}
+	int a;
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+		if (a < 100)
+		       printf(" ");
+		else
+			printf("\n");
+	}
 
-int main()
-{
-fizz_buzz();
-return 0;
+	return 0;
 }
