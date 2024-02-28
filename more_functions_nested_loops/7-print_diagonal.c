853721643f607712f70d524e8cd74db5b73d2a4e
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-02-28 15:18:07 +0000

    7putshalfc

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,15 +5,15 @@
  */
 void print_diagonal(int n)
 {
-	int b;
-	int c;
-	if (n <= 0)
-		_putchar('\n');
-	for (b = 0; b < n; b++)
-	{
-		for (c = 0; c < b; c++)
-			_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
-	}
+int b;
+int c;
+if (n <= 0)
+_putchar('\n');
+for (b = 0; b < n; b++)
+{
+for (c = 0; c < b; c++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
 }
