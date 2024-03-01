1d3639b40e21fe8fc9a588fb465aec151359fce8
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-03-01 12:31:10 +0000

    100atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,31 @@
 #include "main.h"
+
 /**
- * _atoi - convert string to int
- * @s: string to convert
- * Return: integer
+ *_atoi - convert string to int
+ *@s: string to conver
+ *Return: integer
  */
 int _atoi(char *s)
 {
 int sign;
 unsigned int num;
 char *temp;
+
 temp = s;
 num = 0;
 sign = 1;
-while (*temp != '\0' && (*temp < '0' || *temp > '9')) 
+while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 {
-if (*temp == '-') 
-{
+if (*temp == '-')
 sign *= -1;
-}
 temp++;
 }
-if (*temp != '\0') 
+if (*temp != '\0')
 {
-do 
-{
+do {
 num = num * 10 + (*temp - '0');
 temp++;
-} 
-while (*temp >= '0' && *temp <= '9');
+} while (*temp >= '0' && *temp <= '9');
 }
 return (num * sign);
 }
