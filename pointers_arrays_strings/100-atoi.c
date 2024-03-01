94aa6e375ee735981a14c7ca541e0ce78f8a1db0
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-03-01 09:29:01 +0000

    100ATOIC

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,23 +3,26 @@
  * _atoi - converts string to integer
  * @s: string input
  *
- * Return: integer value of string
+ * Return: integer value of the string
  */
 int _atoi(char *s)
 {
-int result = 0; 
-int sign = 1; 
-int pos = 0; 
-while (s[pos])
-{
-if (s[pos] == '-')
-sign = sign * -1;
-if (s[pos] >= '0' && s[pos] <= '9')
-{
-result = result * 10 + (s[pos] - '0');
-if (s[pos + 1] < '0' || s[pos + 1] > '9') 
-break; 
-pos++; 
-}
-return result * sign; 
+	int result = 0; 
+	int sign = 1;   
+	int pos = 0;    
+	while (s[pos]) 
+	{       
+		if (s[pos] == '-') 
+		{
+			sign *= -1;
+		}
+		if (s[pos] >= '0' && s[pos] <= '9')
+		{       
+			result = result * 10 + (s[pos] - '0'); 
+			if (s[pos + 1] < '0' || s[pos + 1] > '9')  
+				break;  
+		}    
+		pos++; 
+	}    
+	return result * sign; 
 }
