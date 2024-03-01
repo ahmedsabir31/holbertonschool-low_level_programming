eb2aaa4d682fe1af6536ced04b719858e19429b0
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-03-01 12:33:45 +0000

    100atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *_atoi - convert string to int
  *@s: string to conver
@@ -10,7 +9,6 @@ int _atoi(char *s)
 int sign;
 unsigned int num;
 char *temp;
-
 temp = s;
 num = 0;
 sign = 1;
@@ -22,10 +20,12 @@ temp++;
 }
 if (*temp != '\0')
 {
-do {
+do 
+{
 num = num * 10 + (*temp - '0');
 temp++;
-} while (*temp >= '0' && *temp <= '9');
+}
+while (*temp >= '0' && *temp <= '9');
 }
 return (num * sign);
 }
