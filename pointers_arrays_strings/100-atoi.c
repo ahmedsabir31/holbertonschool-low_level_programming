5a5b66ca457f9115b833fcac9cc6230395c895e3
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-03-01 09:36:54 +0000

    100ATOIC

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,22 +7,22 @@
  */
 int _atoi(char *s)
 {
-	int result = 0; 
-	int sign = 1;   
-	int pos = 0;    
-	while (s[pos]) 
-	{       
-		if (s[pos] == '-') 
-		{
-			sign *= -1;
-		}
-		if (s[pos] >= '0' && s[pos] <= '9')
-		{       
-			result = result * 10 + (s[pos] - '0'); 
-			if (s[pos + 1] < '0' || s[pos + 1] > '9')  
-				break;  
-		}    
-		pos++; 
-	}    
-	return result * sign; 
+int result = 0; 
+int sign = 1;   
+int pos = 0;    
+while (s[pos]) 
+{       
+if (s[pos] == '-') 
+{
+sign *= -1;
+}
+if (s[pos] >= '0' && s[pos] <= '9')
+{       
+result = result * 10 + (s[pos] - '0'); 
+if (s[pos + 1] < '0' || s[pos + 1] > '9')  
+break;  
+}    
+pos++; 
+}    
+return result * sign; 
 }
