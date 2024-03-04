de44dada62e2a14f894ff17986ae2e7f0767a7c0
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-03-04 09:02:50 +0000

    0memsetc

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- *_menset - function that fills memory with a constant byte.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
- * Return: string
- */
+*_memset - The _memset() function fills
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: string.
+*/
 char *_memset(char *s, char b, unsigned int n)
-for (unsigned int i = 0; i < n; i++) {
-        s[i] = b;
-    }
-    return s;
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
