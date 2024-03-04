e54b7a5c72fd4d6f7e1227b0ef7c8478e9e6c50d
Ahmed Sabir <ahmed.sbr31@gmail.com>
2024-03-04 10:40:07 +0000

    2strchtr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string
+ * @c: char
+ * @s: string
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	do
+{
+	if (*s != c)
+		s++;
+	else if(*s == c)
+		return (s);
+}
+	while (*s);
+
+	
+	return (s);
+
+	return ('\0');
+}
+
